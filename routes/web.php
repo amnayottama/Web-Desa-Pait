4c73b2d12441db1d21a11ca4beecaeab7a5f7e8d
charissaida <charis.saida@gmail.com>
2024-10-09 17:59:33 +0700

    add page profile

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.dashboard');
 });
+Route::get('/profile', function () {
+    return view('admin.profile');
+});
 Route::get('/login', function () {
     return view('login');
 });
